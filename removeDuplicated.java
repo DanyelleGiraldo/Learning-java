7c190e3bed2af42ca11c2bf43ac6fabb2eb2efb6
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-04-04 14:12:28 -0500

    Algorithm to remove duplicate elements from an array.

--- removeDuplicated.java
+++ removeDuplicated.java
@@ -1,6 +1,17 @@
 class removeDuplicated {
     public int removeDuplicates(int[] nums) {
-        int [] nums= {1,2,3,2,1};
-        
+        if(nums.length == 0){
+            return 0;
+        }
+
+        int uniquitecount = 1;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[uniquitecount] = nums[i];
+                uniquitecount++;
+            }
+        }
+        return uniquitecount;
     }
 }
